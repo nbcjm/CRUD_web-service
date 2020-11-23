9cd04da5a2d4fc49799093dd4a00711a056a4d6f
최정민 <legolas90099009@gmail.com>
2020-11-23 17:39:24 +0900

    add notepad dir

---
+++ notepad/src/test/java/com/zeronsoftn/cjm/notepad/NotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeronsoftn.cjm.notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
