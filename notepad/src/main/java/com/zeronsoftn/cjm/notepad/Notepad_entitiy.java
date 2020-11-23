5ce0d5eab8b881a8f61dfd98d2dbddc4bad5e0bd
최정민 <legolas90099009@gmail.com>
2020-11-23 17:43:29 +0900

    add entitiy

---
+++ notepad/src/main/java/com/zeronsoftn/cjm/notepad/Notepad_entitiy.java
@@ -0,0 +1,17 @@
+package com.zeronsoftn.cjm.notepad;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Notepad_entitiy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+
+    private String description;
+}
